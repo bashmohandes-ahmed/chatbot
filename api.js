6c4fbabfcd21272e253d2292e14b85e410eddbaa
Ahmed Farag <ahmedusamaofficial1@gmail.com>
2026-04-16 04:50:44 +0200

    Update and rename api.js to main.py

--- api.js
+++
@@ -1,9 +0,0 @@
-export async function sendMessage(text) {
-  const res = await fetch("http://localhost:8000/chat", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ text })
-  });
-
-  return res.json();
-}
